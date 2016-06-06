f7f76bce02fa50b52bf24eb33e862a5411a78cf3
pratiksarvaiya <sarvaiya.pratik@gmail.com>
2016-06-07 03:49:28 +0530

    added dummy handler for eclipse plugin

--- com.deber.api/src/com/deber/api/Dummy.java
+++ com.deber.api/src/com/deber/api/Dummy.java
@@ -1,5 +1,18 @@
 package com.deber.api;
 
-public class Dummy {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+public class Dummy implements RequestStreamHandler {
+
+	@Override
+	public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
